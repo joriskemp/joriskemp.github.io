3b79fe5dfec766d80b6ba30590adde7c4f894230
joriskemp <joris@kemperink.com>
2014-11-16 08:23:57 +0100

    Update platformer.js

--- 2014/Quintus/jorisgame/platformer.js
+++ 2014/Quintus/jorisgame/platformer.js
@@ -187,7 +187,7 @@ Q.scene("level2",function(stage) {
   stage.insert(new Q.Enemy({ x: 760, y: 333 }));
  
   // dit is de schatkist x=rechts en y =van boven naar beneden
-  stage.insert(new Q.Tower({ x: 2250, y: 560, }));
+  stage.insert(new Q.Tower({ x: 2210, y: 540, }));
 });
 
 
