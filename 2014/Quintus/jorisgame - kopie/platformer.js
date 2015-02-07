96fa08697e51426d6293caa6fc014504081d6a0c
joriskemp <joris@kemperink.com>
2015-02-07 07:38:56 +0100

    Update platformer.js

--- 2014/Quintus/jorisgame - kopie/platformer.js
+++ 2014/Quintus/jorisgame - kopie/platformer.js
@@ -17,7 +17,7 @@ function scoreBijwerken(){
 }
 
 window.addEventListener("load",function() {
-<audio autoplay> <source src="muziek/grapes_-_I_dunno.mp3" type="audio/mpeg"> <source src="muziek/grapes_-_I_dunno.ogg" type="audio/ogg"> </audio> - See more at: http://www.mijn-eigen-website.nl/achtergrondmuziek.html#sthash.i6kJj97n.dpuf
+<audio autoplay> <source src="muziek/grapes_-_I_dunno.mp3" type="audio/mpeg"> <source src="muziek/grapes_-_I_dunno.ogg" type="audio/ogg"> </audio> 
 scoreBijwerken();
 
 // Set up an instance of the Quintus engine  and include
